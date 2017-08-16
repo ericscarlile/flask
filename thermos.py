6d902cb439a76bb03b008da9f60b60789b667469
Eric <ericscarlile@gmail.com>
2017-08-16 00:47:43 -0500

    Create 'add.html' to add social bookmarks. Create base.html page for a base template to be extended from. Refactor templates to extend base.html. Add context to templates.

--- thermos.py
+++ thermos.py
@@ -2,11 +2,28 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def initials(self):
+        return ('{}. {}.'.format(self.first_name[0].upper(), self.last_name[0].upper()))
+
+    def __str__(self):
+        return ('{} {}'.format(self.first_name.title(), self.last_name.title()))
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="This is the context title!",
+                           user=User('eric', 'carlile'))
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run()
